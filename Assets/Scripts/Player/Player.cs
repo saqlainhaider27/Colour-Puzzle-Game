48100c6bf2718a6b114343abf6f0f368037e30ec
Saqlain Haider <saqlainhaider27@gmail.com>
2024-10-22 21:11:26 +0500

    MovementFix

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 
 public class Player : Singleton<Player> {
 
+    [Header("Swipe Settings")]
+    [SerializeField] private float minimumDistance = .2f;
+    [SerializeField] private float maximumTime = 1f;
+    [SerializeField, Range(0f, 1f)] private float directionThreshold = .9f;
+
+    private Vector2 startPosition;
+    private float startTime;
+
+    private Vector2 endPosition;
+    private float endTime;
+
+    private Vector2 direction2D;
+
     [Header("Player Settings")]
     [SerializeField] private float moveSpeed;
     private Vector2 moveDirection ;
-    private Vector2 newMoveDirection;
-
-    [SerializeField] private float raycastLength = 0.5f;
+    private Vector2 newMoveDirection; // As player changes direction after coming out of teleport point this is the new direction on teleport
     [SerializeField] private List<GameObject> playerMeshes = new List<GameObject>();
 
     [Header("References")]
-    [SerializeField] private SwipeDetection swipeDetection;
     [SerializeField] private LayerMask collisionLayer;
+    [SerializeField] private Colour currentColour;
 
     private TrailRenderer[] trailRenderers;
 
@@ -27,47 +38,72 @@ public class Player : Singleton<Player> {
 
     private bool generated = false; 
     private bool canMove;
-    [SerializeField] private Colour currentColour;
 
     private void Start() {
         currentColour = GetComponentInChildren<PlayerColour>().GetCurrentPlayerMeshColour();
-
         trailRenderers = GetComponentsInChildren<TrailRenderer>(true);
-
     }
     private void Update() {
-
-
-        if (swipeDetection.GetSwipeDirection() != Vector2.zero) {
-            moveDirection = swipeDetection.GetSwipeDirection();
-        }
-        // UpdateMoveDirection();
+        // Debug.Log(moveDirection);
         canMove = CanMove();
 
         if (canMove) {
+            // Debug.Log("CanMove");
             MovePlayer();
             RotateInMoveDirection(); // Rotate object in move direction
         }
         else {
+            // Debug.Log("Cant Move");
             StopPlayer();
         }
 
     }
 
 
-    private void UpdateMoveDirection() {
-        if (newMoveDirection != moveDirection && newMoveDirection != Vector2.zero) {
-            moveDirection = newMoveDirection;
-            newMoveDirection = Vector2.zero;
-        }
-        else {
-            if (swipeDetection.GetSwipeDirection() != Vector2.zero) {
-                moveDirection = swipeDetection.GetSwipeDirection();
-            }
-        }
+    private void OnEnable() {
+        InputManager.Instance.OnStartTouch += SwipeStart;
+        InputManager.Instance.OnEndTouch += SwipeEnd;
+    }
 
-        Debug.Log(moveDirection);
+    private void OnDisable() {
+        InputManager.Instance.OnStartTouch -= SwipeStart;
+        InputManager.Instance.OnEndTouch -= SwipeEnd;
+    }
 
+    private void SwipeStart(Vector2 position, float time) {
+        startPosition = position;
+        startTime = time;
+    }
+
+    private void SwipeEnd(Vector2 position, float time) {
+        endPosition = position;
+        endTime = time;
+        DetectSwipe();
+    }
+
+    private void DetectSwipe() {
+        if (Vector3.Distance(startPosition, endPosition) >= minimumDistance && (endTime - startTime) <= maximumTime) {
+            Debug.DrawLine(startPosition, endPosition, Color.red, 5f);
+            Vector3 direction = endPosition - startPosition;
+            direction2D = (Vector2)direction.normalized;
+            // direction2D = new Vector2(direction.x, direction.y).normalized;
+            SwipeDirection(direction2D);
+        }
+    }
+
+    private void SwipeDirection(Vector2 direction) {
+        if (Vector2.Dot(Vector2.up, direction) > directionThreshold) {
+            moveDirection = Vector2.up;
+        }
+        else if (Vector2.Dot(Vector2.down, direction) > directionThreshold) {
+            moveDirection = Vector2.down;
+        }
+        else if (Vector2.Dot(Vector2.left, direction) > directionThreshold) {
+            moveDirection = Vector2.left;
+        }
+        else if (Vector2.Dot(Vector2.right, direction) > directionThreshold) {
+            moveDirection = Vector2.right;
+        }
     }
     private bool CanMove() {
         return CheckForCollidersInPath() && moveDirection != Vector2.zero;
@@ -80,26 +116,36 @@ public class Player : Singleton<Player> {
 
     private void StopPlayer() {
         moveSpeed = 0;
-        //moveDirection = Vector2.zero; // Stop player movement
-        //swipeDetection.SetSwipeDirection(moveDirection);
+        moveDirection = Vector3.zero;
     }
+    
     private bool CheckForCollidersInPath() {
-        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, moveDirection, raycastLength, collisionLayer);
+
+        // Use squarecast or something
+        Vector2 size = new Vector2(0.4f, 0.4f);
+        float length = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(transform.position, size, 0, moveDirection, length, collisionLayer);
+        //RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, moveDirection, raycastLength, collisionLayer);
 
         if (raycastHit) {
+            Debug.Log(raycastHit.collider.name);
             if (CheckWallCollision(raycastHit)) {
                 return false;
             }
 
-            if (CheckPaintCollision(raycastHit))
+            if (CheckPaintCollision(raycastHit)) {
                 return true;
-            if (CheckTeleportPointCollision(raycastHit))
+            }
+
+            if (CheckTeleportPointCollision(raycastHit)) {
                 return true;
+            }
+
 
             return false;
         }
 
-        return CheckWinPointProximity();
+        return CheckWinPointProximity(); 
     }
     private bool CheckWallCollision(RaycastHit2D raycastHit) {
         if (raycastHit.collider.TryGetComponent<Wall>(out Wall collidedWall)) {
@@ -112,9 +158,7 @@ public class Player : Singleton<Player> {
                 // The player's color matches the wall's color; play particle effect
 
                 if (moveDirection == Vector2.zero) {
-
                     PlayWallCollisionParticles();
-
                 }
             }
             else {
@@ -169,7 +213,6 @@ public class Player : Singleton<Player> {
                 // Move player towards modifiedMoveDirection from TeleportPoint
                 // Player teleported so set trail to true
                 moveDirection = newMoveDirection;
-                swipeDetection.SetSwipeDirection(moveDirection);
                 MovePlayer();
             }
 
@@ -262,5 +305,13 @@ public class Player : Singleton<Player> {
     }
     public void SetCurrentPlayerColour(Colour setColour) {
         currentColour = setColour;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(transform.position, new Vector2(0.5f, 0.5f));
+        Vector3 endPoint =  (Vector2)transform.position+ moveDirection * 0.5f;
+        Gizmos.DrawWireCube(endPoint, new Vector2(0.5f, 0.5f));
     }
 }
