71c8c24e6fe1556aae93008b90f92e3b9d97d5af
saqlainhaider2700 <138958018+saqlainhaider27@users.noreply.github.com>
2025-07-26 14:01:12 +0500

    Add Scene Switcher utility and update scenes
    Introduced a new 'Scenes Switcher' utility with a floating window for switching scenes at runtime. Added related scripts, metadata, and documentation. Updated '11.unity' and 'Main.unity' scenes to integrate the utility and made minor scene adjustments. Updated EditorBuildSettings to include the new utility.

---
+++ Assets/Scenes Switcher/ScenesSwitcher.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+static class SceneSwitchInit {
+#if UNITY_EDITOR
+	[UnityEditor.InitializeOnLoadMethod]
+#endif
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad),]
+	static void Init() {
+		SceneManager.sceneLoaded += SceneSwitcher.Init1;
+	}
+}
+
+class SceneSwitcher : MonoBehaviour {
+	public Rect WindowRect;
+	Rect _windowRect0;
+	string _sceneLabel;
+
+	int _windowId;
+	bool _dirty;
+	static bool _initialized;
+	float _dpiInv;
+	const string Title = "Scene Switcher";
+
+	internal static void Init1(Scene cur, LoadSceneMode _) {
+		SceneManager.sceneLoaded -= Init1;
+		if (_initialized)
+			return;
+		_initialized = true;
+
+		var go = new GameObject(Title) {
+			// hideFlags = HideFlags.HideInHierarchy
+		};
+		DontDestroyOnLoad(go);
+		var ss = go.AddComponent<SceneSwitcher>();
+		ss._windowId = GUIUtility.GetControlID(FocusType.Keyboard);
+		ss._windowRect0 = ss.WindowRect = new Rect(0, 0, 40, 51);
+		SceneManager.sceneLoaded += ss.Init2;
+		ss.Init2(cur, _);
+	}
+
+	void OnDestroy() {
+		SceneManager.sceneLoaded -= Init2;
+	}
+
+	void Init2(Scene activeScene, LoadSceneMode _) {
+		_sceneLabel = string.Format("{0}:{1}", activeScene.buildIndex, activeScene.name);
+		_dpiInv = 1f / Screen.dpi / 1.5f;
+		if (Screen.dpi == 264f) // not editor GameView
+			_dpiInv /= 1.5f;
+		_dirty = true;
+	}
+
+	const float MinWidth = 120f;
+	const float BaseWidth = 40f;
+
+	void OnGUI() {
+		if (_dirty) {
+			_dirty = false;
+			var labelSize = GUI.skin.label.CalcSize(new GUIContent {text = _sceneLabel});
+			
+			WindowRect.width = Mathf.Max(MinWidth, BaseWidth + labelSize.x);
+			WindowRect.height = _windowRect0.height;
+		}
+
+#if !UNITY_EDITOR
+        ScaleGUI();
+#endif
+
+		WindowRect = GUILayout.Window(_windowId, WindowRect, InnerGui, Title);
+	}
+
+	void ScaleGUI() {
+		var max = Mathf.Max(Screen.width, Screen.height);
+		var ratio = max * _dpiInv;
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(ratio, ratio, 1));
+	}
+
+	void InnerGui(int _) {
+		using (new GUILayout.HorizontalScope()) {
+			var activeScene = SceneManager.GetActiveScene();
+			var valid = SceneManager.sceneCountInBuildSettings > activeScene.buildIndex && SceneManager.GetSceneByBuildIndex(activeScene.buildIndex) == activeScene;
+			var buildIndex = valid ? activeScene.buildIndex : -1;
+			bool buf;
+			int newIndex;
+
+
+			GUILayout.BeginVertical();
+			{
+				GUILayout.Label(_sceneLabel);
+				GUILayout.BeginHorizontal();
+				{
+					buf = GUI.enabled;
+					newIndex = buildIndex - 1;
+					//GUI.enabled = newIndex >= 0;
+					if (newIndex < 0) {
+						newIndex = SceneManager.sceneCountInBuildSettings - 1;
+					}
+					if (GUILayout.Button(" < "))
+						SceneManager.LoadScene(newIndex);
+
+					GUI.enabled = buf;
+
+					buf = GUI.enabled;
+					newIndex = buildIndex + 1;
+					//GUI.enabled = newIndex <= SceneManager.sceneCountInBuildSettings - 1;
+					if (newIndex > SceneManager.sceneCountInBuildSettings - 1) {
+						newIndex = 0;
+					}
+					if (GUILayout.Button(" > "))
+						SceneManager.LoadScene(newIndex);
+
+					GUI.enabled = buf;
+				}
+				GUILayout.EndHorizontal();
+			}
+			GUILayout.EndVertical();
+		}
+
+		GUI.DragWindow();
+	}
+}
---
+++ Assets/Scenes Switcher/readme.txt
@@ -0,0 +1,16 @@
+Scenes Switcher v1.0
+
+Importing new version of an asset:
+1) Delete an old version of the product - Assets/Scene Switcher folder
+2) Import from Asset Store to Assets folder
+
+How to launch Scene Switcher
+1) Launch your project on the device
+2) The floating window of Scene Switcher will show automatically in the top-left corner
+3) Drag the window as you need
+4) Switch scenes with the arrow buttons on the screen
+
+- Product video available on the product page in Asset Store
+http://u3d.as/1Sm4
+- Support channel available here:
+ https://t.me/joinchat/BdWObFckBlxZOW06Unnv6w
